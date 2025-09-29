d5e6327005363cecb683fc660bd43131599b64fc
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-09-29 17:23:19 -0400

    update

--- produx.js
+++ produx.js
@@ -5,7 +5,7 @@
         {
             id: 'SB64-IBJF-001',
             name: 'SuperBox64 Ice Blue Jack Frost',
-            description: '20,000+ games, 500GB SSD, 8GB RAM',
+            description: 'Game Console, 500GB SSD, 8GB RAM',
             price: 599.99,
             image: './images/wintermix.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/EGZCL9AURBV9G'
@@ -13,7 +13,7 @@
         {
             id: 'SB64-CBLT-002',
             name: 'SuperBox64 Classic Blue LED Trak',
-            description: '20,000+ games, 500GB SSD, 8GB RAM',
+            description: 'Game Console, 500GB SSD, 8GB RAM',
             price: 599.99,
             image: './images/superbox64_classic.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/9S6SLHCS9BUWQ'
@@ -21,7 +21,7 @@
         {
             id: 'SB64-SMYT-003',
             name: 'SuperBox64 Retro64 Yellow Trak',
-            description: '20,000+ games, 250GB SSD, 4GB RAM',
+            description: 'Game Console, 250GB SSD, 4GB RAM',
             price: 499.99,
             image: './images/superbox64_yellowtrak.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/WCMSTDZWA9KS2'
@@ -29,7 +29,7 @@
         {
             id: 'SB64-NSWT-004',
             name: 'SuperBox64 Super64 White Trak',
-            description: '20,000+ games, 250GB SSD, 4GB RAM',
+            description: 'Game Console, 250GB SSD, 4GB RAM',
             price: 499.99,
             image: './images/superbox64_whitetrak.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/MJA2JFTKJZBNJ'
