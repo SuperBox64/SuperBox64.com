3ab68f3c288105cb59113a6ca8a19c383a7b58bb
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-23 21:24:47 -0500

    SuperBox64.com

--- produx.js
+++ produx.js
@@ -6,7 +6,7 @@
             id: 'SB64-IBJF-001',
             name: 'SuperBox64 Ice Blue Jack Frost',
             description: 'Raspberry Pi5 game console with SuperBox64OS, 500GB SSD, 8GB RAM',
-            price: 599.99,
+            price: 499.99,
             image: './images/wintermix.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/EGZCL9AURBV9G'
         },
@@ -14,7 +14,7 @@
             id: 'SB64-CBLT-002',
             name: 'SuperBox64 Classic Blue LED Trak',
             description: 'Raspberry Pi5 game console with SuperBox64OS, 500GB SSD, 8GB RAM',
-            price: 599.99,
+            price: 499.99,
             image: './images/superbox64_classic.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/9S6SLHCS9BUWQ'
         },
@@ -22,7 +22,7 @@
             id: 'SB64-SMYT-003',
             name: 'SuperBox64 Retro64 Yellow Trak',
             description: 'Raspberry Pi5 game console with SuperBox64OS, 250GB SSD, 4GB RAM',
-            price: 499.99,
+            price: 399.99,
             image: './images/superbox64_yellowtrak.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/WCMSTDZWA9KS2'
         },
@@ -30,7 +30,7 @@
             id: 'SB64-NSWT-004',
             name: 'SuperBox64 Super64 White Trak',
             description: 'Raspberry Pi5 game console with SuperBox64OS, 250GB SSD, 4GB RAM',
-            price: 499.99,
+            price: 399.99,
             image: './images/superbox64_whitetrak.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/MJA2JFTKJZBNJ'
         },
@@ -38,15 +38,15 @@
             id: 'GRS-PPSP-005',
             name: 'GRS Push Pull Spinner',
             description: 'GRS Push Pull Arcade Spinner',
-            price: 60.00,
+            price: 99.99,
             image: './images/grs_push_pull_spinner.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/Z3AM4TWMVZB3J'
         },
         {
             id: 'SB64-TKBL-006',
             name: 'SuperBox64 Trackball Standalone',
-            description: 'Black Red Blue, Light up LED Blue Trackball ball. USB out port on back, LED Light up trackball, 7 foot USB cable, 4 Super rubber feet. Max 2 items',
-            price: 159.99,
+            description: 'Black Lid Matte Finish, Black Red Blue, White Trackball ball. USB out port on back, Color: Glossy White Trackball and Arcade Convex buttons, Left Middle and Right Click, 8 foot USB black cable, 4 Super rubber feet. Max 2 items',
+            price: 129.99,
             image: './images/superbox64_trackball.jpeg',
             paypalLink: 'https://www.paypal.com/ncp/payment/WXE8WKD4PTQ72'
         }
