41273dab493f8deb7a62f1759869052345c4fdf7
WebAuthn FIDO3 AI <admin@starplayrx.com>
2025-11-23 21:36:04 -0500

    ProduxJS 2026

--- produx.js
+++ produx.js
@@ -3,20 +3,20 @@
     // Products data
     const products = [
         {
-            id: 'SB64-IBJF-001',
-            name: 'SuperBox64 Ice Blue Jack Frost',
-            description: 'Raspberry Pi5 game console with SuperBox64OS, 500GB SSD, 8GB RAM',
-            price: 499.99,
-            image: './images/wintermix.jpeg',
-            paypalLink: 'https://www.paypal.com/ncp/payment/EGZCL9AURBV9G'
+            id: 'GRS-PPSP-005',
+            name: 'GRS Push Pull Spinner',
+            description: 'GRS Push Pull Arcade Spinner',
+            price: 99.99,
+            image: './images/grs_push_pull_spinner.jpeg',
+            paypalLink: 'https://www.paypal.com/ncp/payment/Z3AM4TWMVZB3J'
         },
         {
-            id: 'SB64-CBLT-002',
-            name: 'SuperBox64 Classic Blue LED Trak',
-            description: 'Raspberry Pi5 game console with SuperBox64OS, 500GB SSD, 8GB RAM',
-            price: 499.99,
-            image: './images/superbox64_classic.jpeg',
-            paypalLink: 'https://www.paypal.com/ncp/payment/9S6SLHCS9BUWQ'
+            id: 'SB64-TKBL-006',
+            name: 'SuperBox64 Trackball Standalone',
+            description: 'Black Lid Matte Finish, Black Red Blue, White Trackball ball. USB out port on back, Color: Glossy White Trackball and Arcade Convex buttons, Left Middle and Right Click, 8 foot USB black cable, 4 Super rubber feet. Max 2 items',
+            price: 129.99,
+            image: './images/superbox64_trackball.jpeg',
+            paypalLink: 'https://www.paypal.com/ncp/payment/WXE8WKD4PTQ72'
         },
         {
             id: 'SB64-SMYT-003',
@@ -35,20 +35,20 @@
             paypalLink: 'https://www.paypal.com/ncp/payment/MJA2JFTKJZBNJ'
         },
         {
-            id: 'GRS-PPSP-005',
-            name: 'GRS Push Pull Spinner',
-            description: 'GRS Push Pull Arcade Spinner',
-            price: 99.99,
-            image: './images/grs_push_pull_spinner.jpeg',
-            paypalLink: 'https://www.paypal.com/ncp/payment/Z3AM4TWMVZB3J'
+            id: 'SB64-IBJF-001',
+            name: 'SuperBox64 Ice Blue Jack Frost',
+            description: 'Raspberry Pi5 game console with SuperBox64OS, 500GB SSD, 8GB RAM',
+            price: 499.99,
+            image: './images/wintermix.jpeg',
+            paypalLink: 'https://www.paypal.com/ncp/payment/EGZCL9AURBV9G'
         },
         {
-            id: 'SB64-TKBL-006',
-            name: 'SuperBox64 Trackball Standalone',
-            description: 'Black Lid Matte Finish, Black Red Blue, White Trackball ball. USB out port on back, Color: Glossy White Trackball and Arcade Convex buttons, Left Middle and Right Click, 8 foot USB black cable, 4 Super rubber feet. Max 2 items',
-            price: 129.99,
-            image: './images/superbox64_trackball.jpeg',
-            paypalLink: 'https://www.paypal.com/ncp/payment/WXE8WKD4PTQ72'
+            id: 'SB64-CBLT-002',
+            name: 'SuperBox64 Classic Blue LED Trak',
+            description: 'Raspberry Pi5 game console with SuperBox64OS, 500GB SSD, 8GB RAM',
+            price: 499.99,
+            image: './images/superbox64_classic.jpeg',
+            paypalLink: 'https://www.paypal.com/ncp/payment/9S6SLHCS9BUWQ'
         }
     ];
 
@@ -59,13 +59,13 @@
 
         products.forEach(product => {
             const productCard = `
-                <div class="product-card">
+                <div class="product-card" onclick="window.open('${product.paypalLink}', '_blank')" style="cursor: pointer;">
                     <img src="${product.image}" alt="${product.name}">
                     <div class="product-content">
                         <h3>${product.name}</h3>
                         <p>${product.description}</p>
                         <div class="price">$${product.price}</div>
-                        <a href="${product.paypalLink}" class="buy-button">Buy Now</a>
+                        <a href="${product.paypalLink}" class="buy-button" onclick="event.stopPropagation();" target="_blank">Buy Now</a>
                     </div>
                 </div>
             `;
